fc2e3717e204d7b114bf8320466a114a29ff5471
Sam9100 <mhaitsamia@gmail.com>
2024-06-11 10:29:24 +0700

    import func and add new func

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+import {postJSON,getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
+
 // Add a scroll event listener to change the navbar background on scroll
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
@@ -7,6 +10,31 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+window.onload = function()
+{
+    rc = Cookie.get('message'); //rc == result cookie
+
+    if (rc.boolean === true) {
+        var data = JSON.parse(decodeURIComponent(rc.data));
+        alert(data.body.replace('+', ' '));
+    }
+}
+
+function actionfunctionname(){
+    let user={
+        email:getValue("email"),
+        githubusername:getValue("githubusername"),
+        gitlabusername:getValue("gitlabusername"),
+        githostusername:getValue("githostusername")
+    };
+    if (getCookie("login")===""){
+        redirect("/signin");
+    }else{
+        postJSON(backend.user.data,"login",getCookie("login"),user,responseFunction);
+        hide("buttonkirimaccount");
+    }  
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('login-form');
@@ -47,16 +75,3 @@ document.addEventListener('DOMContentLoaded', function() {
         alert(`Password reset link sent to ${resetEmail}`);
     });
 });
-
-function redirectToDashboard() {
-    // Lakukan validasi login di sini (jika ada)
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    if (email === "admin@admin.com" && password === "admin") {
-        alert('Login successful!');
-        window.location.href = 'dashboard-admin.html';
-    } else {
-        alert('Invalid email or password.');
-    }
-}
