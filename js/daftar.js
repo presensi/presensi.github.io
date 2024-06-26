d71ed2ed231510ee55b44a8fc5ee0f627e45cd34
Sam9100 <mhaitsamia@gmail.com>
2024-06-26 14:16:18 +0700

    new func

--- js/daftar.js
+++ js/daftar.js
@@ -47,4 +47,16 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
-  
+
+
+  document.getElementById('showPassword').addEventListener('change', function() {
+    const passwordField = document.getElementById('password');
+    const confirmPasswordField = document.getElementById('confirm_password');
+    if (this.checked) {
+        passwordField.type = 'text';
+        confirmPasswordField.type = 'text';
+    } else {
+        passwordField.type = 'password';
+        confirmPasswordField.type = 'password';
+    }
+});
