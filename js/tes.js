3ee18acfdd28726d7cf709e37ce78f49b5b8d3c1
Sam9100 <mhaitsamia@gmail.com>
2024-07-21 12:51:18 +0700

    tes #1

--- js/tes.js
+++ js/tes.js
@@ -6,10 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
             data.forEach(presensi => {
                 const tr = document.createElement('tr');
                 tr.innerHTML = `
-                    <td>${presensi.Lokasi.Nama}</td>
-                    <td>${presensi.Lokasi.Batas.coordinates[0]}</td>
-                    <td>${presensi.Lokasi.Batas.coordinates[1]}</td>
                     <td>${presensi.PhoneNumber}</td>
+                    <td>${presensi.Lokasi.Nama}</td>
                     <td>${new Date(presensi.CreatedAt).toLocaleString()}</td>
                 `;
                 tbody.appendChild(tr);
